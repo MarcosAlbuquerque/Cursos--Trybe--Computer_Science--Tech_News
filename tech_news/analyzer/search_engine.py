c4f7b39ad32495915b540bb313d2b59458abe316
Marcos Albuquerque <mpdsa20@gmail.com>
2022-03-04 06:36:13 -0300

    Requisito 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,19 +1,35 @@
-from tech_news.database import find_news
-from datetime import date
+from datetime import datetime
+from tech_news.database import find_news, search_news
 
 # Requisito 6
 def search_by_title(title):
     new = find_news()
-    filter = [
-        new for new in new if new["title"].lower() == title.lower()
-    ]
+    filter = [new for new in new if new["title"].lower() == title.lower()]
     return [(new["title"], new["url"]) for new in filter]
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        format_data = "%Y-%m-%d"
+        data = datetime.strptime(date, format_data)
+        data_to_str = datetime.strftime(data, format_data)
+        get_all = search_news(
+            {
+                "timestamp": {
+                    "$regex": data_to_str,
+                    "$options": "i",
+                }
+            }
+        )
 
+        n_filter = [item for item in get_all]
+        return [(new["title"], new["url"]) for new in n_filter]
+    except ValueError:
+        raise ValueError("Data inválida")
+
+
+print(search_by_date("2020-11-11"))
 
 # Requisito 8
 def search_by_source(source):
