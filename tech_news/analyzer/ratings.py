3b83c3fae04c4d1a1b87811b35fd230512c16105
Marcos Albuquerque <mpdsa20@gmail.com>
2022-03-04 08:38:38 -0300

    Requisito 10 e 11

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,8 +1,32 @@
+from tech_news.database import get_collection
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    get = get_collection().aggregate(
+        [
+            {
+                "$project": {
+                    "title": 1,
+                    "url": 1,
+                }
+            },
+            {"$limit": 5},
+        ]
+    )
+    n_tulpa = [new for new in get]
+    return [(new["title"], new["url"]) for new in n_tulpa]
 
 
 # Requisito 11
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    get = get_collection().aggregate(
+        [
+            {"$project": {"categories": 1}},
+            {"$unwind": "$categories"},
+            {"$group": {"_id": "$categories", "count": {"$sum": 1}}},
+            {"$sort": {"_id": 1}},
+            {"$limit": 5},
+        ]
+    )
+    return [category["_id"] for category in get]
